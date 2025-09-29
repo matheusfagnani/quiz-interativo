a1d9c55ba17b0c263e5c5cd130e806faf069826d
gustavo rodrigues <gustavolourenco821@gmail.com>
2025-09-29 08:46:15 -0300

    estrutura e limpeza do projeto quiz

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import questions from './data/questions'
+import './App.css'
+
+function App() {
+  const [tela, setTela] = useState(0)
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/data/questions.js
@@ -0,0 +1,13 @@
+export default questions = {
+    questao1: {
+        enunciado: "Quem é o presidente do brasil",
+        alternativas: {
+            a: "Lula",
+            b: "Bolsonaro",
+            c: "Dilma",
+            d: "Ratinho",
+            e: "Pablo Marçal",
+            alternativa_correta: "Lula"
+        }
+    }
+}
